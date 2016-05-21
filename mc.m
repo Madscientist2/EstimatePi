983fcbf52f84f9cc543c6e50b87f6a881daff9d8
Abdullah Abbas <abdullahabbas@airbears2-10-142-100-171.airbears2.1918.berkeley.edu>
2016-05-20 19:48:46 -0700

    I slightly modified and ensured the tab is only 2 character so Yusuf doesn't get pissed off on me hehe

--- mc.m
+++ mc.m
@@ -1,10 +1,10 @@
-% MC estimate of pi
+% Monte Carlo estimate of pi
 close all; clear all; clc
+
 %Define variables to use to estimate the number of points that gets inside a circle that is inscribed in a sqaure
 n=1000; %Number of points, the more, the more accurate is the estimate
 x=rand(1,n); %To create random numbers between 0-1
 y=rand(1,n); %To create random numbers between 0-1
-
 a=0; %To count the points inside the circle 
 b=0; %To count the points outside the circle plus the square
 
@@ -18,9 +18,11 @@ for i=1:n
 		plot(x(i), y(i),'ko') %To plot the points outside the quadrant
 	end
 end
+
 %We know the area of square= 4*R^2
 %Also the area of the circle= Pi*R^2
 %Dividing the two area we get: A=Pi/4
 %Since we are randomly throwing points to the circle which also gets to the square, we can get an estimate ratio of the area (which I called A above)
 ratio=a/b; %Ratio
 pi_=4*ratio
+%Credit: For more information, please visit http://polymer.bu.edu/java/java/montepi/MontePi.html
