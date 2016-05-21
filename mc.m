bd95759c20e1d57395d6dc4b38af1a91ec5824fa
Abdullah Abbas <abdullahabbas@airbears2-10-142-100-171.airbears2.1918.berkeley.edu>
2016-05-20 19:39:43 -0700

    I have add this matlab code that estimate Pi using Monte Carlo

---
+++ mc.m
@@ -0,0 +1,26 @@
+% MC estimate of pi
+close all; clear all; clc
+%Define variables to use to estimate the number of points that gets inside a circle that is inscribed in a sqaure
+n=1000; %Number of points, the more, the more accurate is the estimate
+x=rand(1,n); %To create random numbers between 0-1
+y=rand(1,n); %To create random numbers between 0-1
+
+a=0; %To count the points inside the circle 
+b=0; %To count the points outside the circle plus the square
+
+for i=1:n
+	b=b+1;
+	if x(i)^2 + y(i)^2 <=1 %This condition to account for points inside the circile since we assume the cicle has radius=1
+		a=a+1;
+		plot(x(i),y(i),'ro'); %To plot the points inside the quadrant
+		hold on;
+	else
+		plot(x(i), y(i),'ko') %To plot the points outside the quadrant
+	end
+end
+%We know the area of square= 4*R^2
+%Also the area of the circle= Pi*R^2
+%Dividing the two area we get: A=Pi/4
+%Since we are randomly throwing points to the circle which also gets to the square, we can get an estimate ratio of the area (which I called A above)
+ratio=a/b; %Ratio
+pi_=4*ratio
